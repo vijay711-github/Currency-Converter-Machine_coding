5a3784607fe5b1278a16c2dcc7d21384824c3132
vijay711-github <vj7112000@gmail.com>
2023-09-06 23:40:51 +0530

    Custom Hook

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className="text-3xl bg-orange-500">Vijay</h1>
+      
+     </>
+  )
+}
+
+export default App
